3e05373a5b4a3b338b28496912d9dc67434e3316
Barsbekteam <barsbekbarsbek99@gmail.com>
2023-08-11 11:31:35 +0300

    first commit

---
+++ src/testing/index.test.js
@@ -0,0 +1,33 @@
+import axios from 'axios';
+import configureMockStore from 'redux-mock-store';
+import thunk from 'redux-thunk';
+import * as actions from './../store/actionCreators';
+
+const middlewares = [thunk];
+const mockStore = configureMockStore(middlewares);
+
+
+
+
+describe('async actions', () => {
+    it('creates FETCH_SUCCESS when fetching data has been done', () => {
+        const responseData = { some: 'data' };
+        axios.get = jest.fn().mockResolvedValue({ data: responseData });
+
+
+
+
+        const expectedActions = [
+            { type: 'FETCH_SUCCESS', payload: responseData },
+        ];
+
+        const store = mockStore({});
+
+
+
+
+        return store.dispatch(actions.fetchingStock()).then(() => {
+            expect(store.getActions()).toEqual(expectedActions);
+        });
+    });
+});
